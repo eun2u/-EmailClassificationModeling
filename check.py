1a60a279e125d5f655ef698d7953daf60e364a29
Jihae Na <njnjh0317@gmail.com>
2020-12-06 17:13:52 +0900

    여러 단어 입력받게 수정

--- check.py
+++ check.py
@@ -1,8 +1,9 @@
 from gensim.models import KeyedVectors
 
 loaded_model = KeyedVectors.load_word2vec_format("data")
-a=input("단어를 입력해주세요 : ")
-model_result=loaded_model.most_similar(a, topn=30)
-#topn 을 정의해주면 출력 갯수를 지정할 수 있다.
-#기본값은 10
-print(model_result)
+while(True):
+    a=input("단어를 입력해주세요 : ")
+    model_result=loaded_model.most_similar(a, topn=30)
+    #topn 을 정의해주면 출력 갯수를 지정할 수 있다.
+    #기본값은 10
+    print(model_result)
