c95203cc7365b6c1dbeaf01b420467f1bd3ada61
Jihae Na <njnjh0317@gmail.com>
2020-12-08 18:06:54 +0900

    [#4]파일명 입력 받도록 수정

--- data_preprocessing/data_preprocessing_news.py
+++ data_preprocessing/data_preprocessing_news.py
@@ -1,22 +1,21 @@
 import kss
 
 a = input("문장으로 구분할 파일 이름을 입력하세요(.txt 제외) : ")
-f = open("./"+a+".txt","r")
-f2 = open("./file_"+a+".txt","w")
+f = open("./preprocessing_phase1/"+a+".txt","r")
+a = a + "_phase2"
+f2 = open("./preprocessing_phase2/"+a+".txt","w")
 line = f.readline()
 num = 0
 while(line):
     num+=1
-    if(num%100000==0):
+    if(num%100==0):
         print(num)
     s = line.replace("\n","")
     for sent in kss.split_sentences(s):
         sent=sent.replace("\n","")
         if(sent.endswith(".")):  
-            print("if")
             f2.write(sent+"\n")
         else:
-            print("else")
             f2.write(sent+".\n")
     line = f.readline()
 f.close()
