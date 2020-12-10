34cc77126afb5dcada3ae100f29e13630e503d08
eun2u <hhaneunjoo@gmail.com>
2020-12-10 19:14:27 +0900

    [#6] divide daumMail to daum and kakao

--- crawling/daumMail_crawling.py
+++ crawling/daumMail_crawling.py
@@ -4,14 +4,29 @@ import time
 from bs4 import BeautifulSoup
 from secret import DAUM
 
-def login_to_daum():
-    #다음 카카오 중 카카오로 로그인
+def login_to_kakao():
+    #다음, 카카오 중 카카오로 로그인
     login_url = 'https://accounts.kakao.com/login?continue=https%3A%2F%2Flogins.daum.net%2Faccounts%2Fksso.do%3Frescue%3Dtrue%26url%3Dhttps%253A%252F%252Fwww.daum.net%252F'
     driver.get(login_url)
+    
     id_input = driver.find_element_by_name('email')
     id_input.send_keys(myId)
 
-    pwd_input = driver.find_element_by_name('password')
+    pwd_input = driver.find_element_by_name('password')    
+    pwd_input.send_keys(myPass)
+
+    driver.find_element_by_xpath('//*[@id="login-form"]/fieldset/div[8]/button[1]').click()
+    time.sleep(1)
+
+def login_to_daum():
+    #다음, 카카오 중 다음으로 로그인
+    login_url='https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F'
+    driver.get(login_url)
+
+    id_input = driver.find_element_by_name('id')
+    id_input.send_keys(myId)
+
+    pwd_input = driver.find_element_by_name('pw')
     pwd_input.send_keys(myPass)
 
     driver.find_element_by_xpath('//*[@id="login-form"]/fieldset/div[8]/button[1]').click()
@@ -79,8 +94,8 @@ myId=DAUM["id"]
 myPass=DAUM["password"]
 
 driver=webdriver.Chrome('/Users/han-eunju/Downloads/chromedriver')
-
-login_to_daum()
+login_to_kakao
+#login_to_daum()
 move_to_mailbox()
 get_mail_list()
 prevent_close()
