ab850147aed36249c41f7383d61c7322d521e9ef
Jihae Na <njnjh0317@gmail.com>
2020-12-15 00:40:20 +0900

    [#7]

--- test_classification.py
+++ test_classification.py
@@ -27,7 +27,7 @@ def checkfile_list_in_folder(folderName, index_info, testfolderName):
             file_list = os.listdir(path_dir)
             for i in file_list:
                 full_file_list[i] = num
-    print(len(full_file_list))
+    # print(len(full_file_list))
     for i in dir_list2:
         if i not in full_file_list.keys():
             full_file_list[i] = 1
@@ -35,21 +35,31 @@ def checkfile_list_in_folder(folderName, index_info, testfolderName):
 def check(list1, list2):
     total_num = 100
     num=0
-    print(len(list2))
+    # print(len(list2))
     for i in list2.keys():
         if(list1[i] == list2[i]):
             num+=1
-    print(str(num/total_num*100)+"%")
+    return str(num/total_num*100)
         
 if __name__ == "__main__":
 
     index_info, full_file_list = testfile_list_in_folder("test2") #인덱스 정보 받아오기
     # print(full_file_list)
-    testfoldername = input("테스트한 데이터가 있는 폴더 : ")
+    # while(True):
+    #     result = []
+    #     for i in range(4):
+    #         testfoldername = "test_data"+str(i+1)
+    #         foldername = input("결과를 비교할 폴더 이름 : ")
+    #         full_file_list2 = checkfile_list_in_folder(foldername, index_info, testfoldername)
+    #         #print(index_info)
+    #         #print(full_file_list2)
+    #         result.append(check(full_file_list, full_file_list2))
+
+    #     print(result)
+    #     print("평균 : "+ str(sum(result)/float(len(result)))+"%")
+    testfoldername = input("테스트할 폴더 : ")
     foldername = input("결과를 비교할 폴더 이름 : ")
     full_file_list2 = checkfile_list_in_folder(foldername, index_info, testfoldername)
-    print(index_info)
-    print(full_file_list2)
-    check(full_file_list, full_file_list2)
-
-
+    #print(index_info)
+    #print(full_file_list2)
+    print(check(full_file_list, full_file_list2)+"%")
